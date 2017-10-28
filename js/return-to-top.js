d2bf1509910692923bc39731274544dc64dcec21
dmitmel <dmytro.meleshko@gmail.com>
2017-10-28 10:56:09 +0300

    add callback for changing window.location.hash in SmoothScroll.scroll

--- js/return-to-top.js
+++ js/return-to-top.js
@@ -2,23 +2,25 @@ createModule('ReturnToTop', function() {
   var SmoothScroll = require('SmoothScroll');
 
   var $window = $(window);
-  var $returnToTop = $('.return-to-top');
+  var returnToTop = $('.return-to-top');
 
   var MIN_SCROLL = $('#about').offset().top;
   var VISIBLE_CLASS = 'return-to-top_visible';
 
   $window.scroll(function() {
     if ($window.scrollTop() >= MIN_SCROLL) {
-      $returnToTop.addClass(VISIBLE_CLASS);
+      returnToTop.addClass(VISIBLE_CLASS);
     } else {
-      $returnToTop.removeClass(VISIBLE_CLASS);
+      returnToTop.removeClass(VISIBLE_CLASS);
     }
   });
 
-  var HEADER_OFFSET = $('#header').offset().top;
-
-  $returnToTop.click(function(e) {
+  var header = $('#header');
+  returnToTop.click(function(e) {
     e.preventDefault();
-    SmoothScroll.scroll(HEADER_OFFSET);
+    var headerOffset = header.offset().top;
+    SmoothScroll.scroll(headerOffset, function() {
+      window.location.hash = '#';
+    });
   });
 });
--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,16 +1,18 @@
 createModule('SmoothScroll', function(SmoothScroll) {
   var scrollRoot = $('html, body');
-  SmoothScroll.scroll = function(top) {
-    scrollRoot.animate({ scrollTop: top }, 1000, 'easeInOutSine');
+  SmoothScroll.scroll = function(top, callback) {
+    scrollRoot.animate({ scrollTop: top }, 1000, 'easeInOutSine', callback);
   };
 
   SmoothScroll.init = function(link) {
     var target = $(link.hash);
     if (target.length) {
-      var targetOffset = target.offset().top;
       $(link).click(function(e) {
         e.preventDefault();
-        SmoothScroll.scroll(targetOffset);
+        var targetOffset = target.offset().top;
+        SmoothScroll.scroll(targetOffset, function() {
+          window.location.hash = link.hash;
+        });
       });
     }
   };
