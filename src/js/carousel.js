23c6f227103f05a750005a22debfbfd6043bbc12
dmitmel <dmytro.meleshko@gmail.com>
2017-11-01 08:53:54 +0200

    add icons to quotes

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,6 +3,7 @@ $(() =>
     cssEase: 'ease-in-out',
     easing: 'easeInOut',
     prevArrow: `<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>`,
-    nextArrow: `<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>`
+    nextArrow: `<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>`,
+    draggable: false
   })
 );
